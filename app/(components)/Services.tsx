d27ea7883bb4d056a89aa2bc8c34a7e41c8c5eaf
Safwan Sadid <safwan55.ah@gmail.com>
2026-02-23 19:58:03 +0600

    feat: add Services component and integrate it into the Home page with FadeIn animation

---
+++ app/(components)/Services.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SectionTitle from './util/SectionTitle';
+
+const Services = () => {
+  return (
+    <div className='flex items-center p-4 font-raleway pl-12 pr-12'>
+      <SectionTitle text='Services I Offer' />
+    </div>
+  );
+};
+
+export default Services;
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 import Hero from "@/app/(components)/Hero";
 import FollowCursor from "./(components)/Mouse/FollowCursor";
 import FadeIn from "./(components)/Animations/FadeIn";
+import Services from "./(components)/Services";
 
 export default function Home() {
   return (
     <div>
       <FollowCursor />
-      <FadeIn>
+      <FadeIn className="mb-12">
         <Hero />
+      </FadeIn>
+      <FadeIn className="mb-12">
+        <Services />
       </FadeIn>
     </div>
   );
