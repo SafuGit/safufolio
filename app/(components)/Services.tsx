71102654f65a83a95b0915339c58e6cf8fc12f93
Safwan Sadid <safwan55.ah@gmail.com>
2026-02-23 20:04:06 +0600

    feat: add MagicBento component with particle effects and spotlight functionality

--- app/(components)/Services.tsx
+++ app/(components)/Services.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
-import SectionTitle from './util/SectionTitle';
+"use client";
+import SectionTitle from "./util/SectionTitle";
+import MagicBento from "../(reactbits)/Bento";
 
 const Services = () => {
   return (
-    <div className='flex items-center p-4 font-raleway pl-12 pr-12'>
-      <SectionTitle text='Services I Offer' />
+    <div className="flex flex-col items-start p-4 font-raleway pl-12 pr-12">
+      <SectionTitle text="Services I Offer" />
+      <div className="flex mt-8 gap-2">
+        <MagicBento
+          textAutoHide={true}
+          enableStars
+          enableSpotlight
+          enableBorderGlow={true}
+          enableTilt={false}
+          enableMagnetism={false}
+          clickEffect
+          spotlightRadius={630}
+          particleCount={12}
+          glowColor="163, 230, 53"
+          disableAnimations={false}
+        />
+      </div>
     </div>
   );
 };
