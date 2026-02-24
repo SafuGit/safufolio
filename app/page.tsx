c4e3cd2df2321efe1edd8f588ab1259d196652a0
Safwan Sadid <safwan55.ah@gmail.com>
2026-02-24 13:15:19 +0600

    feat: add WorkExperience component and integrate it into the main page; update next.config for image handling

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import HomeComponent from "./components/Home";
+import WorkExperience from "./components/WorkExperience";
 
 export default function Home() {
   return (
     <main>
       <HomeComponent />
+      <WorkExperience />
     </main>
   );
 }
