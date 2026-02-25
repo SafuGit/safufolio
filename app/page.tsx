bcc42636ce233ba6631d8aaf5a948e3d4ff118e1
Safwan Sadid <safwan55.ah@gmail.com>
2026-02-25 12:22:30 +0600

    chore: update dependencies in package.json
    - Added class-variance-authority@^0.7.1
    - Added radix-ui@^1.4.3
    - Added shadcn@^3.8.5
    - Added tw-animate-css@^1.4.0

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
-import HomeComponent from "./components/Home";
-import WorkExperience from "./components/WorkExperience";
-
 export default function Home() {
   return (
     <main>
-      <HomeComponent />
-      <WorkExperience />
+      
     </main>
   );
 }
